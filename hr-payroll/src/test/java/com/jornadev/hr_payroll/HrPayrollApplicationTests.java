90468603ace4edb4e0c7bd8db53ca2a14fa27a36
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-04-10 13:43:01 -0300

    [feat] | hr-payroll implementing (mock)

---
+++ hr-payroll/src/test/java/com/jornadev/hr_payroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jornadev.hr_payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
